0b51b5bc0c0294e7e385000169bf09bd311e1812
Oloklhrwmenos <56202590+Oloklhrwmenos@users.noreply.github.com>
2022-01-20 17:24:22 +0200

    Απλοποίηση της κλάσης, πλέον δεν αλλάζει εσωτερικά το ρολόι

--- src/FCFS.java
+++ src/FCFS.java
@@ -1,45 +1,22 @@
 
+
 public class FCFS extends Scheduler {
 
     public FCFS() {
         /* TODO: you _may_ need to add some code here */
-
-        /* Μεταβλήτη για επιπλέον έλεγχο στην περίπτωση που η
-         * τελευταία διεργασία που έχει φορτωθεί στη μνήμη
-         * τελειώσει την εκτέλεση της ενώ μια άλλη περιμένει
-         * για χώρο στη μνήμη.
-         * (Δεν ειμαι σιγουρος αν ο έλεγχος ειναι περιττός) */
-        boolean outOfProcesses = false;
-
-        while (processes.size() > 0 || outOfProcesses==false) {
-
-            if(processes.size()==0 && outOfProcesses==false){
-                outOfProcesses=true;
-                continue;
-            }
-
-            Process nextProcess = getNextProcess(); //Παίρνω την επόμενη διεργασία που θέλω να εκτελέσω
-            CPU.clock += nextProcess.getBurstTime(); //Την εκτελώ πάντα μέχρι το τέλος
-            nextProcess.waitInBackground(); //Την βάζω στο background (τερματίζοντας τη)
-            CPU.clock += 0; //Ο χρόνος μετάβασης απο την κατάσταση RUNNING στην TERMINATED είναι μηδενικός
-            removeProcess(nextProcess); //Διαγράφω τη διεργασία
-        }
     }
 
     public void addProcess(Process p) {
         /* TODO: you need to add some code here */
 
-        p.getPCB().setState(ProcessState.READY,CPU.clock); //Βάζω την διεργασία σε κατάσταση READY
-        CPU.clock++; //Για τη μετάβαση απο NEW σε READY απαιτείται ένας κύκλος ρολογιου
-        processes.add(p); //Προσθέτω τη διεργασία στη λίστα
+        p.getPCB().setState(ProcessState.READY,CPU.clock); //Change the state to READY
+        processes.add(p); //Add the process to the list
     }
     
     public Process getNextProcess() {
         /* TODO: you need to add some code here
          * and change the return value */
-
-        Process nextProcess = processes.get(0); //Διαλέγω πάντα την πρώτη διεργασία στην ουρά
-        nextProcess.run();  //Τρέχω τη διεργασία
+        Process nextProcess = processes.get(0); //Choose the first process in the queue
         return nextProcess;
     }
 }
